39a369590c762bafa0f7e559730a8ea54a1168f8
Ragul <ragulempire2kk3@gmail.com>
2026-01-20 19:00:15 +0530

    Signup Route is Configured

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,48 @@
-export const login=(req,res)=>{
-    res.send("Signup route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup=async(req,res)=>{
+    try {
+    const {fullname,email,password}=req.body;
+    if(!fullname||!email||!password){
+        return res.status(400).json({message:"All fields are required"});
+    }
+    if(password.length<6){
+        return res.status(400).json({message:"Password must be at least 6 characters long"});
+    }
+    const existingUser=await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({message:"User already exists"});
+    }
+    //const salt=await bcrypt.genSalt(10);
+    //const hashedPassword=await bcrypt.hash(password,salt)
+    const hashedPassword=await bcrypt.hash(password,10)
+    const newUser=new User({
+        fullname,
+        email,
+        password:hashedPassword
+    })
+   if(newUser){
+      generateToken(newUser._id,res);
+      await newUser.save();
+        return  res.status(201).json({
+            _id:newUser._id,
+            fullname:newUser.fullname,
+            email:newUser.email,
+            profilePic:newUser.profilePic,
+        
+        })
+   }
+    else{
+        return res.status(400).json({message:"Invalid user data"});
+    }    
+    } catch (error) {
+        return res.status(500).json({message:"Internal server error",error});
+    }
+    
 }
-export const signup=(req,res)=>{
+export const login=(req,res)=>{
     res.send("Signup route");
 }
 export const logout=(req,res)=>{
--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app=express();
 
 const PORT=process.env.PORT || 5001;
-
+app.use(express.json());
 app.use('/api/auth',authRouter);
 
 connectDB().then(()=>{
