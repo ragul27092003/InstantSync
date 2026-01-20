106b5a229d794fc90cd83136bb6cee88d8e625d7
Ragul <ragulempire2kk3@gmail.com>
2026-01-20 17:45:15 +0530

    Schema , Auth Routes Created

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const login=(req,res)=>{
+    res.send("Signup route");
+}
+export const signup=(req,res)=>{
+    res.send("Signup route");
+}
+export const logout=(req,res)=>{
+    res.send("Logout route");
+}
---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+import authRouter from './routes/auth.route.js';
+
+
+
+dotenv.config();
+
+const app=express();
+
+const PORT=process.env.PORT || 5001;
+
+app.use('/api/auth',authRouter);
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+       console.log(`Server is running on port : ${PORT}`);
+    })})
+.catch((error)=>{
+    console.log("Error in DB connection",error);
+    process.exit(1);
+   })
