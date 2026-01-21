6a2321c7408d77b96d26bc598b64323d79c8c879
Ragul <ragulempire2kk3@gmail.com>
2026-01-21 12:39:32 +0530

    Fix CORS configuration for frontend-backend communication

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 
 dotenv.config();
 
@@ -13,6 +13,10 @@ const app=express();
 const PORT=process.env.PORT || 5001;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true,
+}))
 app.use('/api/auth',authRoutes);
 app.use('/api/message',messageRoutes)
 
